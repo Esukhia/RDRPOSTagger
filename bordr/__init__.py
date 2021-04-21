ac4ea8cb7b5698ef1b4f6c2384b9e23152a432a7
tenzin <kaldantenzin@gmail.com>
2021-04-21 12:50:29 +0530

    Update __init__.py

--- bordr/__init__.py
+++ bordr/__init__.py
@@ -4,4 +4,4 @@ from .pSCRDRtagger.RDRPOSTagger import rdr
 from .Utility.Eval import evaluate
 from .Utility.Config import NUMBER_OF_PROCESSES, THRESHOLD
 
-__version__ = "0.1.4"
+__version__ = "0.1.3"
